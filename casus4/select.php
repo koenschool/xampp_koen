4bdf81977d4b5a50d7f05bf78951f6cdd964a91c
koen <9023005@student.zadkine.nl>
2024-06-07 12:55:24 +0200

    Update select.php

--- casus4/select.php
+++ casus4/select.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rekenmachine</title>
+    <title>📱rekenmachine</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="stylesheet" href="allcss.css?<?php echo time(); ?>">
     <script defer src="code.js?<?php echo time(); ?>"></script>
-    
 
 </head>
 <body>
